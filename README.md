ae80b23d7a814965901d5c9b5154e78012931f51
Jacob Gable <jacob.gable@gmail.com>
2013-05-06 19:02:16 -0500

    Initial commit, base box with node v0.10.5

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ Ghost-Vagrant
 =============
 
 Vagrant setup for developing Ghost
+
+### Instructions
+
+- Install [VirtualBox](https://www.virtualbox.org/wiki/Downloads) and [Vagrant 1.2.2](http://downloads.vagrantup.com/tags/v1.2.2)
+- Clone this repo
+- Edit the `Vagrantfile` in the root
+    - Change your `GhostSourcePath` to match your environment
+- Run `vagrant up` from the root of the cloned repo.
+- Edit your `hosts` file, add an entry for `local.tryghost.org` pointing to `192.168.33.10`
+- Login to the VM with `vagrant ssh`
+- Run the Ghost App with `cd /Ghost-Web && node index.js`
+
+### License
+
+Copyright 2013 Ghost
---
+++ modules/ghost/manifests/init.pp
@@ -0,0 +1,40 @@
+
+class ghost($node_version = "v0.10.5") {
+    # Add some default path values
+    Exec { path => ['/usr/local/bin','/usr/local/sbin','/usr/bin/','/usr/sbin','/bin','/sbin', "/home/vagrant/nvm/${node_version}/bin"], }
+
+    # Base packages and ruby gems (sass, compass)
+    class { essentials: }
+
+    class { nginx: 
+        require => [Class["essentials"]]
+    }
+
+    # Install node through NVM
+    class { 'nvm':
+        node_version => $node_version,
+        require => [Class["essentials"]]
+    }
+
+    # This function depends on some commands in the nvm.pp file
+    define npm( $directory="/home/vagrant/nvm/${ghost::node_version}/lib/node_modules" ) {
+      exec { "install-${name}-npm-package":
+        unless => "test -d ${directory}/${name}",
+        command => "npm install -g ${name}",
+        require => Exec['install-node'],
+      }
+    }
+    
+    # Global npm modules  
+    npm { ["grunt-cli",
+           "sass",
+           "compass"]:
+    }
+    
+    # Examples of installing packages from a package.json if we need to.
+    #exec { "npm-install-packages":
+    #  cwd => "/home/vagrant/",
+    #  command => "npm install .",
+    #  require => Exec['install-node'],
+    #}
+}
