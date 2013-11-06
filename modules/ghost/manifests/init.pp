72502cdcd8b309746a5d61afc8b520ba76b9b173
Tyler Benziger <tabenziger@gmail.com>
2013-11-06 14:18:57 -0800

    Added manifest to start node server on vagrant up
    closes #29
    - Upstart is used to daemonize node server
    - Upstart starts the server on vagrant up and stops it on vagrant halt
    - Additional puppet exec commands added to perform tasks like `git submodule update --init`, `npm install`, and `grunt init`
    - These tasks ensure the node app will startup smoothly

--- modules/ghost/manifests/init.pp
+++ modules/ghost/manifests/init.pp
@@ -11,6 +11,10 @@ class ghost($node_version = "v0.10.5") {
         require => [Class["essentials"]]
     }
 
+    class { upstart:
+        require => [Class["essentials"]]
+    }
+
     # Install and setup phantomjs and casperjs
     class { casperjs:
         require => [Class["essentials"]]
@@ -45,9 +49,22 @@ class ghost($node_version = "v0.10.5") {
     }
 
     # Examples of installing packages from a package.json if we need to.
-    #exec { "npm-install-packages":
-    #  cwd => "/home/vagrant/",
-    #  command => "npm install .",
-    #  require => Exec['install-node'],
-    #}
+    exec { "npm-install-packages":
+      cwd => "/home/vagrant/code/Ghost",
+      command => "npm install",
+      require => Exec['install-node'],
+    }
+
+    exec { "git submodule update":
+      cwd     => "/home/vagrant/code/Ghost",
+      command => "git submodule update --init",
+      require => Class["essentials"]
+    }
+
+    exec { "grunt init":
+      cwd => "/home/vagrant/code/Ghost",
+      command => "grunt init",
+      require => Exec["npm-install-packages"]
+    }
+
 }
