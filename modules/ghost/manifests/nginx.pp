78d6bd9f09f2f59d0475b2e96e5dbbdd0e94d422
Sebastian Gierlinger <s.gierlinger@me.com>
2014-04-28 17:14:26 +0200

    Add SSL
    - added self-signed ssl certificate (self-signed is intentional to
    prevent people from using it for other environments)
    - changed nginx.conf

--- modules/ghost/manifests/nginx.pp
+++ modules/ghost/manifests/nginx.pp
@@ -9,6 +9,27 @@ class nginx {
     enable => true,
     hasrestart => true,
     subscribe => File['nginx-conf'],
+    require => File['ssl-cert', 'ssl-cert-key', 'nginx-conf'],
+  }
+
+  file { '/etc/nginx/ssl/':
+    ensure => "directory",
+  }
+
+  file { 'ssl-cert':
+    path => "/etc/nginx/ssl/server.crt",
+    source => "puppet:///modules/ghost/config/server.crt",
+    owner  => "root",
+    group  => "root",
+    require => File["/etc/nginx/ssl/"],
+  }
+
+  file { 'ssl-cert-key':
+    path => "/etc/nginx/ssl/server.key",
+    source => "puppet:///modules/ghost/config/server.key",
+    owner  => "root",
+    group  => "root",
+    require => File["/etc/nginx/ssl/"],
   }
 
   file { 'nginx-conf':
